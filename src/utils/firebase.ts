48be09437d5ac7c76e816a1b47697f832d51252f
Hamasif <hamas.fathi@mhs.unsoed.ac.id>
2024-11-14 10:01:44 +0700

    first commit

---
+++ src/utils/firebase.ts
@@ -0,0 +1,18 @@
+// src/utils/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCD48RkWDyYH8Ot7FlflckyTyCQHJNk63Q",
+    authDomain: "vue-firebase-6b1a3.firebaseapp.com",
+    projectId: "vue-firebase-6b1a3",
+    storageBucket: "vue-firebase-6b1a3.firebasestorage.app",
+    messagingSenderId: "884345052115",
+    appId: "1:884345052115:web:43c86292d60b03f4146d26",
+  };
+
+const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider };
